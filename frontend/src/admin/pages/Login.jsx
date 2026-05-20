3930fae20ea63f5d4322240d001813f5f7c73032
dalalanshraj <dalalanshraj@gmail.com>
2026-05-21 04:09:02 +0530

    ok

--- frontend/src/admin/pages/Login.jsx
+++ frontend/src/admin/pages/Login.jsx
@@ -120,7 +120,7 @@ const Login = () => {
         </button>
 
         <p className="text-center text-sm text-gray-500 mt-6">
-          Donna Daniel Realty Admin Panel
+         Nick of Time and Love Shack Admin Panel
         </p>
 
       </form>
