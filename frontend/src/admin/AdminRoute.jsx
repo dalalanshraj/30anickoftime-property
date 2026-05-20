e59618c477a0aae1d7a381e103ed65febd20f061
dalalanshraj <dalalanshraj@gmail.com>
2026-05-21 03:49:15 +0530

    ok

--- frontend/src/admin/AdminRoute.jsx
+++ frontend/src/admin/AdminRoute.jsx
@@ -1,22 +1,37 @@
 import { Navigate } from "react-router-dom";
 
 const AdminRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  const user = JSON.parse(localStorage.getItem("user"));
 
-   if (
-    !user ||
-    (
-      user.role !== "admin" &&
-      user.role !== "superadmin"
-    )
-  ) {
-    return <Navigate to="/admin/login" />;
+  const token = localStorage.getItem("token");
+
+  const user = JSON.parse(
+    localStorage.getItem("user")
+  );
+
+  // NOT LOGGED IN
+  if (!token || !user) {
+    return (
+      <Navigate
+        to="/admin/login"
+        replace
+      />
+    );
   }
 
-  // ✅ allow
+  // NOT ADMIN
+  if (
+    user.role !== "admin" &&
+    user.role !== "superadmin"
+  ) {
+    return (
+      <Navigate
+        to="/admin/login"
+        replace
+      />
+    );
+  }
+
   return children;
 };
-
 
 export default AdminRoute;
